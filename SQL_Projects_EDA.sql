baa26dccaaad44fc6c5f9ba4c4fb9fb638580e6f
azonim811 <136748040+azonim811@users.noreply.github.com>
2023-11-07 10:01:26 -0800

    Add files via upload

--- SQL_Projects_EDA.sql
+++ SQL_Projects_EDA.sql
@@ -1,10 +1,10 @@
 /*
 Covid 19 Data Exploration
-Skills used: JOINs, CTEs, Temp Tables, Window Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: JOIN, CTE, Temporary Tables, Window Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 USE projects;
 
-# See the table
+# Checking the tables
 SELECT *
 FROM covid_deaths;	-- 10081 row(s) returned
 
@@ -24,7 +24,7 @@ DROP COLUMN date;	-- drop the original date column
 ALTER TABLE covid_deaths 
 RENAME COLUMN corrected_date TO date;	-- change the name back to its original
 
-# Set up for most observation
+# Set up for most of the following observation
 SELECT location, date, population, total_cases, total_deaths
 FROM covid_deaths
 ORDER BY 1, 2;
@@ -38,7 +38,7 @@ ORDER BY 1, 2;
 # Looking at total cases against the population
 SELECT location, date, population, total_cases, (total_cases / population) * 100 AS population_percentage	-- population percentage who got contracted
 FROM covid_deaths
-WHERE location = 'United States'	
+WHERE location = 'United States'
 ORDER BY 1, 2;
 
 # List the countries with the highest contraction rate of its population 
@@ -49,7 +49,7 @@ ORDER BY population_percentage DESC;
 
 # List the countries with the highest death count encountered
 SELECT location, MAX(total_deaths) AS highest_deathcount
-FROM covid_deaths					-- results has group of continents in the location
+FROM covid_deaths					-- initial results has group of continents in the location
 WHERE continent IS NOT NULL			-- to resolve this, add WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY highest_deathcount DESC;	
@@ -58,18 +58,19 @@ ORDER BY highest_deathcount DESC;
 SELECT continent, MAX(total_deaths) AS highest_deathcount
 FROM covid_deaths
 GROUP BY continent
-ORDER BY highest_deathcount DESC;	-- the result under North America seems other countries' count is not included
+ORDER BY highest_deathcount DESC;	
 
 # Looking at global numbers
 SELECT date, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage	
 FROM covid_deaths
 WHERE continent IS NOT NULL
 GROUP BY date
-ORDER BY 1;	-- results will be per day if just the global total then take out column date and GROUP BY date
-			-- just the global total then take out column date and GROUP BY date
-			SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage	
-			FROM covid_deaths
-			WHERE continent IS NOT NULL;	
+ORDER BY 1;	-- results will be per day
+
+	-- just the global total, taking out column date and GROUP BY date
+	SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage	
+	FROM covid_deaths
+	WHERE continent IS NOT NULL;	
 
 # Joining the two tables and looking at global population that has been vaccinated 
 SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations
@@ -113,31 +114,60 @@ CREATE TABLE population_vaccinated (
     
 INSERT INTO population_vaccinated
 SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations, 
-		SUM(v.new_vaccinations)	OVER (PARTITION BY d. location ORDER BY d.location, d.date) AS rolling_vaccinations
-		FROM covid_deaths d
-		JOIN covid_vaccination v
-			ON d.location = v.location AND d.date =  v.date
-		WHERE d.continent IS NOT NULL    
-		ORDER BY 2, 3;
+	SUM(v.new_vaccinations)	OVER (PARTITION BY d. location ORDER BY d.location, d.date) AS rolling_vaccinations
+	FROM covid_deaths d
+	JOIN covid_vaccination v
+		ON d.location = v.location AND d.date =  v.date
+	WHERE d.continent IS NOT NULL    
+	ORDER BY 2, 3;
         
 SELECT *, ROUND((rolling_vaccinations / population) * 100, 2) AS rolling_percentage
 FROM population_vaccinated;	
 
-
-
-	
-	
-
-
-# List the countries with the most deaths
-SELECT location, SUM(total_deaths) AS highest_casecount, MAX((total_cases / population)) * 100 AS highest_poprate 
+/*
+More SQL pactices
+Summary of datasets
+*/
+# List the countries with the most Covid deaths
+SELECT location, SUM(total_deaths) AS sum_deaths
 FROM covid_deaths
-GROUP BY location, population
-ORDER BY 4 DESC;	-- or ORDER BY highest_poprate DESC
+WHERE continent IS NOT NULL 
+GROUP BY location
+ORDER BY sum_deaths DESC;
+
+# Calculate the total Covid deaths by continent
+SELECT continent, SUM(total_deaths) AS sum_deaths
+FROM covid_deaths
+GROUP BY continent
+ORDER BY 2 DESC;
+
+# Calculate the total vaccinations administered
+SELECT location, MAX(total_vaccinations) AS highest_vaccinated
+FROM covid_vaccination
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY 2 DESC;
+
+# Find the country with the highest vaccination rate per capita
+SELECT d.location, ROUND(MAX(v.total_vaccinations / d.population) * 100, 2) AS highest_vacrate
+FROM covid_vaccination v
+JOIN covid_deaths d
+	ON v.location = d.location AND v.date = d.date
+WHERE v.location IS NOT NULL 
+GROUP BY d.location
+ORDER BY 2 DESC;
+
+/*
+United States, Brazil, and India: top three on Covid deaths
+Andorra, Seychelles, and Montenegro: top three on contraction rate
+...
+*/
+
+	
+	
 
 
 
-select *
-from covid_deaths
-limit 3;
+
+
 
